9bb82411c6411881e5bd6d17e7b458f87b2ff97a
Pamudu Mandara <pinimal25@gmail.com>
2025-05-01 15:46:18 +0530

    frontend-homepage-creation

--- skillverse-frontend/src/App.js
+++ skillverse-frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
+import CreatePostForm from './components/CreatePostForm';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="bg-gray-100 min-h-screen">
+        <Navbar />
+        <div className="p-6 max-w-3xl mx-auto">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/create" element={<CreatePostForm />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
